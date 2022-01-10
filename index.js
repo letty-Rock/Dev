21a58b23efd405f403323c3656849e76a8772867
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-01-10 14:57:12 -0300

    Banco mysql criado

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+conexao.connect(erro =>{
+    if(erro){
+        console.log(erro)
+    }
+})
 const app = customExpress()
     app.listen(3000, ()=> console.log('servidor rodando na porta 3000'))
     
