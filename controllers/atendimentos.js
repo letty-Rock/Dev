d94650f117cfed3ed38cb31d6ba58bf0e716b673
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-01-10 12:24:56 -0300

    Subindo metodo get e post

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,11 @@
+const res = require("express/lib/response")
+
 module.exports = app =>{
 
-app.get('/atendimentos',(req, res) => res.send('Você está na rota de atendimentos'))}
+app.get('/atendimentos',(req, res) => res.send('Você está na rota de atendimentos'))
+
+
+app.post('/atendimentos', (req, res) =>{console.log(req.body)
+     res.send('Você está na rota de atendimento e está realizando um POST')})
+
+}
