51f58341df75f2225efde4851a98db846f89a4d7
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-01-10 10:48:51 -0300

    Add alterações index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
-
+const consign = require('consign')
 const app = express()
 
+consign()
+.include('controllers')
+.into(app)
     app.listen(3000, ()=> console.log('servidor rodando na porta 3000'))
     
  
