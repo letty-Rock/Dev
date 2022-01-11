b47fb0888b1e66dbef0865bf5bca5d647e721a0f
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-01-11 10:27:48 -0300

    update e delete created

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -7,8 +7,8 @@ app.get('/atendimentos',(req,res) =>{
      Atendimento.lista(res);
 })
 app.get('/atendimentos/:id', (req,res)=>{
-     console.log(req.params);
-     res.sed('OK');
+     const id = parseInt(req.params).id;
+     atendimento.buscaPorId(id, res);
 })
 
 
@@ -16,5 +16,13 @@ app.post('/atendimentos', (req, res) =>{
      const atendimento = req.body
      Atendimento.adiciona(atendimento, res)
     })
-
+    app.patch('/atendimentos/:id', (req,res)=> {
+         const id = parseInt(req.params.id)
+         const valores = req.body
+         Atendimento.altera(id, valores, res)
+    })
+    app.delete('/atendimentos/:id', (req, res)=>{
+         const id = parseInt(req.params.id)
+         atendimento.deleta(id, res)
+    })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -54,7 +54,42 @@ class Atendimento {
                 }
             })
         }
-    }
+        buscaPorId(id, res) {
+            const sql = 'SELECT * FROM Atendimentos WHERE id=&{id}';
+            conexao.query(sql, (erro, resultados)=>{
+                const atendimento = resultados[0];
+                if(erro){
+                    res.status(400).json(erro);                
+                }
+                  else{
+                res.status(200).json(resultados);
+            }})
+        }
+        altera(id, valores, res){
+            if(valores.data){
+                valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+            }
+            const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
+            conexao.query(sql, [valores, id], (erro, resultados) => {
+                if(erro){
+                    res.status(400).json(erro)
+                } else{
+                    res.status(200).json(resultados)
+                }
+            })
+        }
+        deleta(id, res){
+            const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+            conexao.query(sql, id, (erro, resultados)=>{
+                if(erro) {
+                    res.status(400).json(erro)
+                }else{
+                    res.status(200).json({id})
+                }
+            })
+        }
+}
     
 
 module.exports = new Atendimento
